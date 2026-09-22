e3c17af9d242e43a4763666cc67c06e131a79b3d
ahammednibras8 <ahammednibras@proton.me>
2026-09-22 13:36:13 +0530

    Configure ESLint and Prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,35 @@
+import js from "@eslint/js";
+import eslintConfigPrettier, { rules } from "eslint-config-prettier";
+import eslintPluginAstro from "eslint-plugin-astro";
+import tseslint from "typescript-eslint";
+
+export default [
+  {
+    ignores: [
+      ".astro/**",
+      "coverage/**",
+      "dist/**",
+      "node_modules/**",
+      "playwright-report/**",
+      "test-results/**",
+    ]
+  },
+  js.configs.recommended,
+  ...tseslint.configs.strict,
+  ...eslintPluginAstro.configs.recommended,
+  ...eslintPluginAstro.configs["jsx-a11y-strict"],
+  {
+    rules: {
+      "no-eval": "error",
+      "no-implied-eval": "error",
+      "no-new-func": "error",
+    },
+  },
+  {
+    files: ["**/*.astro"],
+    rules: {
+      "astro/no-set-html-directive": "error",
+    },
+  },
+  eslintConfigPrettier,
+]
